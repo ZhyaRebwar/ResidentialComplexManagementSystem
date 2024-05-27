c8ec8c2356ea2ebaba45a7c4d1b9532ca2f24fa1
Zhya Rebwar <ZhyaRebwar9@gmail.com>
2024-05-27 03:05:09 +0300

    update dashboard columns and add number of residents, admin and users

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,11 +21,24 @@ class DashboardController extends Controller
 
         $no_user = User::count();
         // $no_residents = User::roles->exists(['residents'])->count();
+
+        $no_residents = User::join('roles', 'users.id', '=', 'roles.user_id')
+                            ->where('roles.role', 'resident')
+                            ->count();
+                            
+
+        $no_admin = User::join('roles', 'users.id', '=', 'roles.user_id')
+                            ->where('roles.role', 'admin')
+                            ->count();
+                            
      
         return response()->json([
             'house_number' => $no_houses,
             'apartment_number' => $no_apartments,
             'building_number' => $no_buildings,
+            'user_number' => $no_user,
+            'no_residents' => $no_residents,
+            'no_admin' => $no_admin
         ]);
     }
 
